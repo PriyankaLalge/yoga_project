b7366a94d795f2fbc2e1a03975b460cb19f634c2
ganeshavhad <gavhad218@gmail.com>
2017-11-20 17:58:45 +0530

    view package

--- view_packages_api.php
+++ view_packages_api.php
@@ -12,7 +12,7 @@ include 'config.php';
     if ($result->num_rows >0){
      // output data of each row
     while($row = $result->fetch_assoc()){
-   array_push($packages,array('Cat_ID'=>$row['Cat_ID'],'Catogary'=>$row['Catogary'],'Active'=>$row['Active'],'Name_of_plan'=>$row['Name_of_plan'],'Time_unit'=>$row['Time_unit'],'batch'=>$row['batch'],'Description'=>$row['Description']));
+   array_push($packages,array('Cat_ID'=>$row['Cat_ID'],'Catogary'=>$row['Catogary'],'Active'=>$row['Active'],'Name_of_plan'=>$row['Name_of_plan'],'Time_unit'=>$row['Time_unit'],'batch'=>$row['batch'],'Description'=>$row['Description'],'fee'=>$row['fee']));
     }
        $packages_view = array('packages_view'=>$packages);
         echo json_encode($packages_view);
